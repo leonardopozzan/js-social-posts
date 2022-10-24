0bfdbe11045e738d93f2d343c48a5046ea187c90
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-10-24 18:23:49 +0200

    bottone like funzionante

--- js/main.js
+++ js/main.js
@@ -56,36 +56,7 @@ const posts = [
     }
 ];
 
-`<div class="post">
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-            </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">Phil Mangione</div>
-                <div class="post-meta__time">4 mesi fa</div>
-            </div>                    
-        </div>
-    </div>
-    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-    <div class="post__image">
-        <img src="https://unsplash.it/600/300?image=171" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-            </div>
-        </div> 
-    </div>            
-</div>`
+
 const container = document.querySelector('#container');
 for (let object of posts){
     const post = addElementClassHTML('div','post',container);
@@ -109,15 +80,39 @@ for (let object of posts){
     <div class="post__footer">
         <div class="likes js-likes">
             <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
+                <a class="like-button  js-like-button" href="#nogo" data-postid="${object.id}">
                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                     <span class="like-button__label">Mi Piace</span>
                 </a>
             </div>
             <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${object.likes}</b> persone
+                Piace a <b id="like-counter-${object.id}" class="js-likes-counter">${object.likes}</b> persone
             </div>
         </div> 
     </div>    
     `
+}
+
+const btns = document.querySelectorAll('.js-like-button');
+
+for(let element of btns){
+    element.addEventListener('click',handleClick);
+}
+
+
+function handleClick(){
+    const numbOfLikes = document.querySelector(`#like-counter-${this.dataset.postid}`);
+    console.log(numbOfLikes)
+    if(!this.classList.contains('like-button--liked')){
+        this.classList.add('like-button--liked');
+        let numb = parseInt(numbOfLikes.textContent);
+        numb++;
+        numbOfLikes.textContent = numb
+    }else{
+        this.classList.remove('like-button--liked');
+        let numb = parseInt(numbOfLikes.textContent);
+        numb--;
+        numbOfLikes.textContent = numb
+    }
+    
 }
